86b67c87314158151517607650e28066d7d040d7
lucky-sky-777 <nttb749@gmail.com>
2025-12-07 15:16:13 +0700

    Change: test google signin in home page

---
+++ wwwroot/js/auth.js
@@ -0,0 +1,49 @@
+﻿import { auth } from '/js/firebase-init.js';
+import {
+    GoogleAuthProvider,
+    signInWithPopup,
+    signOut,
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+
+const provider = new GoogleAuthProvider();
+
+export async function signInWithGooglePopup() {
+    try {
+        const result = await signInWithPopup(auth, provider);
+
+        const user = result.user;
+        console.log("User signed in:", user.displayName, user.email, user.uid);
+
+        const idToken = await user.getIdToken();
+        console.log("ID Token:", idToken);
+
+        // await fetch('/Account/ExternalSignIn', { method: 'POST', headers: {'Content-Type':'application/json'}, body: JSON.stringify({ idToken }) });
+
+        return {
+            ok: true,
+            user,
+            idToken
+        };
+    } catch (err) {
+        console.error("Sign-in error:", err);
+        return {
+            ok: false,
+            error: err
+        };
+    }
+}
+
+export async function signOutUser() {
+    await signOut(auth);
+}
+
+export function observeAuthState(onChange) {
+    return onAuthStateChanged(auth, (user) => {
+        onChange(user);
+    });
+}
+
+window.signInWithGooglePopup = signInWithGooglePopup;
+window.signOutUser = signOutUser;
+window.observeAuthState = observeAuthState;
---
+++ wwwroot/js/firebase-init.js
@@ -0,0 +1,63 @@
+﻿import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js";
+
+import {
+	ref as dbRef,
+	push as dbPush,
+	set as dbSet,
+	onChildAdded as dbOnChildAdded,
+	onChildChanged as dbOnChildChanged,
+	onChildRemoved as dbOnChildRemoved,
+	onChildMoved as dbOnChildMoved
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyDwe6DIPlE8MiXwR9UarM6WjyphGBDlAb0",
+	databaseURL: "https://dotnet-project-mvc-default-rtdb.firebaseio.com",
+	authDomain: "dotnet-project-mvc.firebaseapp.com",
+	projectId: "dotnet-project-mvc",
+	storageBucket: "dotnet-project-mvc.firebasestorage.app",
+	messagingSenderId: "756217519140",
+	appId: "1:756217519140:web:2a16cf47586e64e1804cb1",
+	measurementId: "G-N27E3WWMRW"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
+
+
+
+export function ref(path) {
+	return dbRef(db, path);
+}
+
+export function push(refNode) {
+	return dbPush(refNode);
+}
+
+export function set(refNode, value) {
+	return dbSet(refNode, value);
+}
+
+
+
+export function onChildAdded(refNode, callback) {
+	return dbOnChildAdded(refNode, callback);
+}
+
+export function onChildChanged(refNode, callback) {
+	return dbOnChildChanged(refNode, callback);
+}
+
+export function onChildRemoved(refNode, callback) {
+	return dbOnChildRemoved(refNode, callback);
+}
+
+export function onChildMoved(refNode, callback) {
+	return dbOnChildMoved(refNode, callback);
+}
+
+export { app, auth, db };
+export default { app, auth, db };
